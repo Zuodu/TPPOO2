2cbefe52ab0ef19230dd041d486cb35c46dc9715
zifanYao <zuoduzuodu24@gmail.com>
2016-12-06 23:04:06 +0100

    commit pour valgrind + makefile

---
+++ Makefile
@@ -0,0 +1,37 @@
+#Makefile du tp2 par Yohan GRACIA et Zifan YAO
+
+CC      = g++
+CFLAGS  = -Wall -c
+OFLAGS = -Wall -o
+
+
+all: TP2Voyage clean
+
+TP2Voyage: Trajet.o TrajetSimple.o TrajetCompose.o Parcours.o Catalogue.o main.o
+	$(CC) $(OFLAGS)-o TPVoyage Trajet.o TrajetSimple.o TrajetCompose.o Parcours.o Catalogue.o main.o
+
+Trajet.o: Trajet.cpp Trajet.h
+	$(CC) $(CFLAGS) Trajet.cpp Trajet.h
+
+TrajetSimple.o: TrajetSimple.cpp TrajetSimple.h
+    $(CC) $(CFLAGS) TrajetSimple.cpp TrajetSimple.h
+
+    TrajetCompose.o: TrajetCompose.cpp TrajetCompose.h
+        $(CC) $(CFLAGS) TrajetCompose.cpp TrajetCompose.h
+
+        Parcours.o: Parcours.cpp Parcours.h
+            $(CC) $(CFLAGS) Parcours.cpp Parcours.h
+
+            Catalogue.o: Catalogue.cpp Catalogue.h
+                $(CC) $(CFLAGS) Catalogue.cpp Catalogue.h
+
+                main.o: main.cpp main.h
+                    $(CC) $(CFLAGS) main.cpp main.h
+
+.PHONY: clean cleanest
+
+clean:
+	rm *.o
+
+cleanest: clean
+	rm TP2Voyage
--- main.cpp
+++ main.cpp
@@ -1,48 +1,17 @@
 using namespace std;
 #include <iostream>
-#include <cstring>
-//#include "TrajetSimple.h"
-//#include "TrajetCompose.h"
 #include "Catalogue.h"
 
-
-void testTrajetSimple()
+void initCatalogue()
 {
-	char* depart = new char [10];
-	char* arrivee = new char [10];
-	depart[0] = 'P';
-	arrivee = strcpy(arrivee, depart);
-	char* transport = new char [3];
-	transport[0] = 'C';
-	transport[1] = 'A';
-	transport[2] = 'R';
-	TrajetSimple* TS1 = new TrajetSimple (0,depart,arrivee,transport);
-	TS1->afficherTrajet();
-}
-
-void testTrajetCompose()
-{
-    char* nomCatalogue = (char*) "katarina";
+    char* nomCatalogue = (char*) "catalogue1";
     Catalogue *myCatalogue = new Catalogue(nomCatalogue);
 	myCatalogue->MenuCatalogue();
 }
 
 int main() {
-  /*  std::cout << "Phase de test testAfficherCatalogue" << std::endl;
-    char depart[10];
-    strcpy(depart,"paris");
-    char arrivee[10];
-    strcpy(arrivee,"marseille");
-    char transport[10];
-    strcpy(transport,"car");
-    char nomCatalogue[10];
-    strcpy(nomCatalogue,"katakata");
-    TrajetSimple *TS1 = new TrajetSimple (depart, arrivee, transport);
-    Catalogue myCatalogue = Catalogue(nomCatalogue);
-    myCatalogue.AddToCatalogueTS(TS1);
-    myCatalogue.AfficherCatalogue();
-    delete TS1;*/
-	testTrajetCompose();
+
+	initCatalogue();
 
     return 0;
 }
