767dd5bafe0ee3b102a9b9f8cc2c952961a53452
Zuodu <zuoduzuodu24@gmail.com>
2016-12-14 15:01:18 +0100

    proposition pour tp3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 using namespace std;
 #include <iostream>
+#include <cstring>
 #include "Catalogue.h"
 
 void initCatalogue()
 {
-    char* nomCatalogue = (char*) "catalogue1";
+    char* nomCatalogue = new char[5];
+    strcpy(nomCatalogue,"cata1");
     Catalogue *myCatalogue = new Catalogue(nomCatalogue);
 	myCatalogue->MenuCatalogue();
     delete myCatalogue;
